4ed2f84f0b19189e1aab10e5df8dcb52016be0de
MarwanRadwan7 <marwan.swe@outlook.com>
2024-04-26 20:51:57 +0300

    Add: Let Statements Parser

---
+++ ast/ast.go
@@ -0,0 +1,52 @@
+package ast
+
+import "dojo/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+
+	// dummy method helps by guiding the Go compiler and possibly causing it to throw errors
+	// when we use a Statement instead of an Expression and vice versa.
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	// dummy method helps by guiding the Go compiler and possibly causing it to throw errors
+	// when we use a Statement instead of an Expression and vice versa.
+	expressionNode()
+}
+
+// Program node is going to be the root node of every AST the parser produces.
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
--- parser/parser.go
+++ parser/parser.go
@@ -1,1 +1,176 @@
 package parser
+
+import (
+	"dojo/ast"
+	"dojo/lexer"
+	"dojo/token"
+	"fmt"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	errors []string
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
+
+	// Read the two tokens, so curr and peek are both set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead.", t, p.curToken.Type)
+	p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{} // parent node
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{
+		Token: p.curToken,
+	}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	//	TODO: We're skipping the expressions until we encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+// expectPeek  checks the type of the peekToken and only if the type is correct does it advance the tokens
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		p.peekError(t)
+		return false
+	}
+}
+
+// function parseProgram() {
+// 	program = newProgramASTNode()
+// 	advanceTokens() --> get more tokens by moving the peek and curr
+// 	for (currentToken() != EOF_TOKEN) {
+// 		statement = null
+// 		if (currentToken() == LET_TOKEN) {
+// 		statement = parseLetStatement()
+// 		} else if (currentToken() == RETURN_TOKEN) {
+// 		statement = parseReturnStatement()
+// 		} else if (currentToken() == IF_TOKEN) {
+// 		statement = parseIfStatement()
+// 		}
+// 		if (statement != null) {
+// 		program.Statements.push(statement)
+// 		}
+// 		advanceTokens()
+// 	}
+// 	return program
+// }
+
+// function parseLetStatement() {
+// 	advanceTokens()
+// 	identifier = parseIdentifier()
+// 	advanceTokens()
+// 	if currentToken() != EQUAL_TOKEN {
+// 		parseError("no equal sign!")
+// 		return null
+// 	}
+// 	advanceTokens()
+// 	value = parseExpression()
+// 	variableStatement = newVariableStatementASTNode()
+// 	variableStatement.identifier = identifier
+// 	variableStatement.value = value
+// 	return variableStatement
+// }
+
+// function parseIdentifier() {
+// 	identifier = newIdentifierASTNode()
+// 	identifier.token = currentToken()
+// 	return identifier
+// }
+
+// function parseExpression() {
+// 	if (currentToken() == INTEGER_TOKEN) {
+// 		if (nextToken() == PLUS_TOKEN)
+// 		{
+// 			return parseOperatorExpression()
+// 		}else if (nextToken() == SEMICOLON_TOKEN) {
+// 			return parseIntegerLiteral()
+// 		}
+// 	} else if (currentToken() == LEFT_PAREN) {
+// 		return parseGroupedExpression()
+// 	}
+// 	// [...]
+// }
+// function parseOperatorExpression() {
+// 	operatorExpression = newOperatorExpression()
+// 	operatorExpression.left = parseIntegerLiteral()
+// 	operatorExpression.operator = currentToken()
+// 	operatorExpression.right = parseExpression()
+// 	return operatorExpression()
+// }
+// 	// [...]
