959febafb84bbf9afbb3b1f98202a0d10f11cde1
MarwanRadwan7 <marwan.swe@outlook.com>
2024-05-14 20:16:42 +0300

    Enhance the Repl

--- main.go
+++ main.go
@@ -8,11 +8,20 @@ import (
 	"dojo/repl"
 )
 
+const DOJO_LOGO = `
+   ____    ___       _   ___  
+  |  _ \  / _ \     | | / _ \ 
+  | | | || | | | _  | || | | |
+  | |_| || |_| || |_| || |_| |
+  |____/  \___/  \___/  \___/ 
+`
+
 func main() {
 	user, err := user.Current()
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println(DOJO_LOGO)
 	fmt.Printf("Hello %s! This is the Dojo programming language!\n",
 		user.Username)
 	fmt.Printf("Feel free to type in commands\n")
